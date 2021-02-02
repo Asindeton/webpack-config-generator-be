640a04199e7f34c274e20a6007f9c656cfda752a
AndreyHub09 <70836292+AndreyHub09@users.noreply.github.com>
2021-02-03 01:36:35 +0300

    fix: generator template fix

--- webpack-config-generator/generator.ts
+++ webpack-config-generator/generator.ts
@@ -97,18 +97,18 @@ const template = "const path = require('path')\n" +
     "        })\n" +
     "    ],\n" +
 
-    "    __devServer__"
+    "    __devServer__" +
     "    module:{\n" +
-    "        rules:[\n"
-    "   {\n"+
-    "       test: /\.css$/,\n"+
-    "       use: cssLoaders()\n"+
-    "   },\n"+
-    "   {\n"+
-    "       test: /\.m?js$/,\n"
-    "       exclude: /node_modules/,\n"
-    "       use: jsLoaders()\n"
-    "   },\n"+
+    "        rules:[\n" +
+    "   {\n" +
+    "       test: /\.css$/,\n" +
+    "       use: cssLoaders()\n" +
+    "   },\n" +
+    "   {\n" +
+    "       test: /\.m?js$/,\n" +
+    "       exclude: /node_modules/,\n" +
+    "       use: jsLoaders()\n" +
+    "   },\n" +
     "   __module_rules__]\n" +
     "    }\n" +
     "}";
