b71f0dcea0a2e11085798e2946b944e0e2d554a4
AndreyHub09 <70836292+AndreyHub09@users.noreply.github.com>
2021-02-02 22:07:27 +0300

    add: auth messege codes were added

--- routes/auth.ts
+++ routes/auth.ts
@@ -21,13 +21,13 @@ router.post(
       try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array(), message: 'Incorrect user data'});
+          return res.status(400).json({ errors: errors.array(), message: 'Incorrect user data', messageCode: 'incorrectUserData'});
         }
         await mongo.connect();
         const { email, password, name } = req.body;
         const userInDb = await User.findOne({ email });
         if (userInDb) {
-          return res.status(400).json({message: 'User has already existed'});
+          return res.status(400).json({message: 'User has already existed', messageCode: 'loginTaken'});
         }
         const passwordHash = await bcrypt.hash(password, saltRounds);
         const user = new User({ email, password: passwordHash, name });
@@ -37,7 +37,7 @@ router.post(
         res.status(201).json({ message: 'User was created', token, userId: user.id, name: user.name });
       }
       catch (e) {
-        res.status(500).json({ message: 'Something goes wrong', error: e })
+        res.status(500).json({ message: 'Something was wrong', error: e, messageCode: 'serverError' })
       }
 });
 
@@ -51,23 +51,23 @@ router.post(
         try {
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
-                return res.status(400).json({ errors: errors.array(), message: 'Incorrect user data'});
+                return res.status(400).json({ errors: errors.array(), message: 'Incorrect user data', messageCode: 'incorrectUserData'});
             }
             await mongo.connect();
             const { email, password } = req.body;
             const userInDb = await User.findOne({ email });
             if (!userInDb) {
-                return res.status(400).json({message: "User doesn't exist"});
+                return res.status(400).json({message: "User doesn't exist", messageCode: 'incorrectUserData'});
             }
             const isMatch = await bcrypt.compare(password, userInDb.password);
             if (!isMatch) {
-                return res.status(400).json({message: "Incorrect input user data"});
+                return res.status(400).json({message: "Incorrect input user data", messageCode: 'incorrectUserData'});
             }
             const token = jwt.sign({ userId: userInDb.id }, jwtSecret, { expiresIn: '24h' });
             res.status(200).json({ token, userId: userInDb.id, name: userInDb.name });
         }
         catch (e) {
-            res.status(500).json({message: 'Something goes wrong'})
+            res.status(500).json({message: 'Something goes wrong', error: e, messageCode: 'serverError'})
         }
     }
 );
